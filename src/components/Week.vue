31c52fbb4f1f06824e50e515d5ac99a06e8f3193
Hunter Harbin <hharbin@gannett.com>
2017-09-13 21:53:45 -0400

    picks

--- src/components/Week.vue
+++ src/components/Week.vue
@@ -2,34 +2,33 @@
     <div class="week">
          <div v-for="(game, index) in games" :key="game.date" class="schedule" :data-game="index">
             <div class="row schedule">
-                 <div @click="pickTeam" :data-team="game.away"  class="col s4 team">
+                <div  :data-team="game.away"  class="col s4 team">
                        <img class="team-logo" :src="_getLogoRef(game.away)">  
-                 </div> 
+                </div> 
                 <div class="col s4">
-                    <!-- <p>@</p> -->
                     <div class="row picks">
                         <div class="spread types">
                             <input class="inline" :name="`groupSpread${index}`" type="radio" :id="`awaySpread${index}`" />
-                            <label class="away inline" :for="`awaySpread${index}`"></label>
+                            <label @click="pickTeamSpread(game.away, index)" :data-team="game.away" class="away inline" :for="`awaySpread${index}`"></label>
                             
                             <div class="vs-text">spread</div>
                             
                             <input :name="`groupSpread${index}`" type="radio" :id="`homeSpread${index}`" />
-                            <label class="home inline" :for="`homeSpread${index}`"></label>
+                            <label @click="pickTeamSpread(game.home, index)" :data-team="game.home" class="home inline" :for="`homeSpread${index}`"></label>
                         </div>
 
                         <div class="straight types">
                             <input class="inline" :name="`groupStraight${index}`" type="radio" :id="`awayStraight${index}`" />
-                            <label class="away inline" :for="`awayStraight${index}`"></label>
+                            <label @click="pickTeamStraight(game.away, index)" :data-team="game.away" class="away inline" :for="`awayStraight${index}`"></label>
                             
                             <div class="vs-text">straight</div>
                             
                             <input class="inline" :name="`groupStraight${index}`" type="radio" :id="`homeStraight${index}`" />
-                            <label class="home inline   ":for="`homeStraight${index}`"></label>
+                            <label @click="pickTeamStraight(game.home, index)" :data-team="game.home" class="home inline":for="`homeStraight${index}`"></label>
                         </div>
                     </div>
                 </div>
-                <div @click="pickTeam" :data-team="game.home" class="col s4 team">
+                <div class="col s4 team">
                      <img class="team-logo" :src="_getLogoRef(game.home)">  
                 </div>
             </div>
@@ -67,7 +66,6 @@ export default {
                     fullGame.favorite = teamJSON[week[game].favorite].name;
                     
                     _games.push(fullGame);
-                    
                 });
             });
         });
@@ -78,28 +76,47 @@ export default {
     methods: {
         onAuthChange: function (user) {
             console.log(user);
-            this.getUserRef(user).then(picks => {
-                console.log(picks);
+            if (user) {
+                this.getUserPromise = this.getUserRef(user).then(userKey => {
+                    this.setCurrentPicks(userKey);
+                });
+            }
+        },
+        setCurrentPicks: function (key) {
+            this.userPicksRef.on('value', picksSnap => {
+                let allWeekPicks = picksSnap.toJSON();
+                Object.keys(allWeekPicks).forEach(gameIndex => {
+                    console.log(allWeekPicks[gameIndex]);
+
+                    // spreads
+                    if (allWeekPicks[gameIndex].spread !== 0) {
+                        document.getElementById('')
+                    }
+                });
             });
         },
         getUserRef: function (user) {
             return new Promise((resolve, reject) => {
                 let userRef = db.ref('users').orderByChild('email').equalTo(user.email).on('value', snap => {
-                    let userKey = Object.keys(snap.toJSON());
-                    console.log(`logged in user: ${userKey}`);
+                    this.userKey = Object.keys(snap.toJSON());
+                    this.userPicksRef = db.ref(`picks/${this.userKey}/week1`);
+                    console.log(`logged in user: ${this.userKey}`);
                     // get the users picks
-                    let pickRef = db.ref(`picks/${userKey}/week1`).on('value', pickSnap => {
-                        resolve(pickSnap.toJSON());
-                    });
+                    resolve(this.userKey);
                 });
             });
         },
-        pickTeam: function (e) {
-            let el = e.currentTarget;
-            el.classList.add('z-depth-5');
-            console.log('picking team..' + el.dataset.team);
-
-            // 
+        pickTeamSpread: function (team, index) {
+            console.log('picking.. ' + team)
+            this.setPick(team, index, 'spread')
+        },
+        pickTeamStraight: function (team, index) {
+            this.setPick(team, index, 'straight')
+        },
+        setPick: function (team, index, type) {
+            this.getUserPromise.then(userKey => {
+                this.userPicksRef.child(`${index+1}/${type}`).set(team);
+            });
         },
         dothing: function () {
             // utils.setPicks();
